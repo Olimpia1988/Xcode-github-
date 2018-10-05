558dd167f85fc853703f9ac42ba314e313cf5677
Olimpia <olimpiacompean@prusuit.org>
2018-10-05 14:54:47 -0400

    initial commit of my xcode proyect

---
+++ Xcode - Github Push/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode - Github Push
+//
+//  Created by Olimpia on 10/5/18.
+//  Copyright © 2018 Olimpia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
